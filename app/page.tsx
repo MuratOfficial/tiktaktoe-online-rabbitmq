1e90d4a3dbfb224e704bc478cc357201c4a25b93
MuratOfficial <toimetm@gmail.com>
2026-02-03 14:11:10 +0500

    feat: set up db

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@/shared/components/ui/button";
-import Image from "next/image";
+import prisma from "@/shared/lib/db";
 
-export default function Home() {
+
+
+export default async function Home() {
+
+const users = await prisma.user.findMany();
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
    <Button>Test</Button>
+   <p>{users[0]?.id}</p>
     </div>
   );
 }
